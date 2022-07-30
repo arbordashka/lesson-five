4a5c4c4c80348bf56cd532462e56f775945dcf0e
arbordashka <dasha_galalu@mail.ru>
2022-07-30 11:40:11 +0300

    Initial commit

---
+++ src/main/java/Person.java
@@ -0,0 +1,19 @@
+public class Person {
+
+    public String name;
+    public String position;
+    public String email;
+    public String phone;
+    public int salary;
+    public int age;
+    public Person (String name, String position, String email, String phone, int salary, int age) {  //коммент-й
+        this.name = name;
+        this.position = position;
+        this.email = email;
+        this.phone = phone;
+        this.salary = salary;
+        this.age = age;
+    }
+
+}
+
